a5eacff4dcda6ad42fb341304c051fcdae5e68aa
DeveloperYamin <developer.yamin@gmail.com>
2022-04-06 16:01:12 +0600

    Background Image !Done

--- App.jsx
+++ App.jsx
@@ -1,16 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.carContainer}>
+        <ImageBackground
+          style={styles.backgroundImage}
+          source={require("./assets/images/ModelS.jpeg")}
+        />
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subtitle}>Starting at $67,450</Text>
         </View>
       </View>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -21,6 +25,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    position: "relative",
   },
   carContainer: {
     height: "100%",
@@ -38,5 +43,11 @@ const styles = StyleSheet.create({
   subtitle: {
     color: "#5c5e62",
     fontSize: 16,
+  },
+  backgroundImage: {
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    resizeMode: "cover",
   },
 });
